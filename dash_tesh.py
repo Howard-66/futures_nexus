00cb0b05e6c7a4b8fc3df0ed0c3ad3dbd13772ae
Howard-66 <xxxxmouse@gmail.com>
2023-12-12 18:37:56 +0800

    design commodity analyzing page

--- dash_tesh.py
+++ dash_tesh.py
@@ -122,24 +122,23 @@ app.layout = dbc.Container(
     Input(component_id='main_chart_dropdown', component_property='value')
 )
 def update_graph(col_chosen):
-    # fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    # 创建主品种数据
     symbol_id = 'RB'
     symbol_name = '螺纹钢'
     fBasePath = 'steel/data/mid-stream/螺纹钢/'
     json_file = './steel/setting.json'
     symbol = commodity.SymbolData(symbol_id, symbol_name, json_file)
     merged_data = symbol.merge_data()
+    # 生成上由原材料的现货和期货价格数据
     symbol_j = commodity.SymbolData('J', '焦炭', json_file)
     data_j = symbol_j.merge_data()
     symbol_i = commodity.SymbolData('I', '铁矿石', json_file)
     data_i = symbol_i.merge_data()
-
     profit_j = data_j[['日期', '现货价格', '主力合约结算价']].copy()
     profit_j.rename(columns={'现货价格': symbol_j.name+'现货', '主力合约结算价': symbol_j.name+'期货'}, inplace=True)
-
     profit_i = data_i[['日期', '现货价格', '主力合约结算价']].copy()
     profit_i.rename(columns={'现货价格': symbol_i.name+'现货', '主力合约结算价': symbol_i.name+'期货'}, inplace=True)
-
+    # 计算品种的现货利润和盘面利润
     formula = symbol.symbol_setting['利润公式']
     df_profit = pd.merge(profit_j, profit_i, on='日期', how='outer')
     df_profit = pd.merge(merged_data[['日期', '现货价格', '主力合约结算价']], df_profit, on='日期', how='outer').dropna(axis=0, how='all', subset=['现货价格', '主力合约结算价'])
@@ -148,71 +147,80 @@ def update_graph(col_chosen):
     df_profit.dropna(axis=0, how='all', subset=['现货利润', '盘面利润'], inplace=True)
     df_append = df_profit[['日期', '现货利润', '盘面利润']].copy()
     symbol.symbol_data = pd.merge(symbol.symbol_data, df_append, on='日期', how='outer')
-
+    # 计算各指标的历史百分位和历史分位数据
     ranked_list = ['库存', '仓单', '现货利润', '盘面利润']
     df_rank = pd.DataFrame()
     for field in ranked_list:
         df_rank = symbol.history_time_ratio(field, df_rank, trace_back_months=60)
-    
-    df_figure = pd.DataFrame()
-    df_figure['基差率颜色'] = symbol.symbol_data['基差率'] > 0
-    df_figure['基差率颜色'] = df_figure['基差率颜色'].replace({True:1, False:0})
-
-    max_y = symbol.symbol_data['主力合约收盘价'] .max() * 1.05
-    min_y = symbol.symbol_data['主力合约收盘价'] .min() * 0.95
-
-    fig = make_subplots(rows=4, cols=1, shared_xaxes=True, specs=[[{"secondary_y": True}], [{"secondary_y": True}], [{"secondary_y": True}], [{"secondary_y": True}]],
-                    vertical_spacing=0.01, subplot_titles=('基差分析', '基差率', '库存/仓单历史分位', '现货利润/盘面利润'), 
+    # 创建基差-库存-利润分析图表
+    fig = make_subplots(rows=4, cols=1, shared_xaxes=True, 
+                        specs=[[{"secondary_y": True}], [{"secondary_y": True}], [{"secondary_y": True}], [{"secondary_y": True}]],
+                    vertical_spacing=0.01, 
+                    subplot_titles=('基差分析', '基差率', '库存/仓单历史分位', '现货利润/盘面利润'), 
                     row_width=[0.1, 0.1, 0.1, 0.7])
 
-    #fig_main = make_subplots(specs=[[{"secondary_y": True}]])
-    fig_future_price = go.Scatter(x=symbol.symbol_data['日期'], y=symbol.symbol_data['主力合约收盘价'], name='期货价格', marker_color='rgb(84,134,240)')
+    # 创建主图：期货价格、现货价格、基差
+    fig_future_price = go.Scatter(x=symbol.symbol_data['日期'], y=symbol.symbol_data['主力合约收盘价'], name='期货价格', 
+                                marker_color='rgb(84,134,240)')
     fig_spot_price = go.Scatter(x=symbol.symbol_data['日期'], y=symbol.symbol_data['现货价格'], name='现货价格', marker_color='rgb(105,206,159)')
-    fig_basis = go.Scatter(x=symbol.symbol_data['日期'], y=symbol.symbol_data['基差'], stackgroup='one', name='基差', marker_color='rgb(239,181,59)')
-    fig_basis.showlegend = False
-    #fig_main.add_trace(fig_future_price)
-    #fig_main.add_trace(fig_basis_rate, secondary_y=True)
-
+    fig_basis = go.Scatter(x=symbol.symbol_data['日期'], y=symbol.symbol_data['基差'], stackgroup='one', name='基差', 
+                        marker_color='rgb(239,181,59)', showlegend=False)
     fig.add_trace(fig_basis, secondary_y=True)
     fig.add_trace(fig_future_price, row = 1, col = 1)
     fig.add_trace(fig_spot_price, row = 1, col = 1)
 
-    fig_basis_rate = go.Bar(x=symbol.symbol_data['日期'], y = symbol.symbol_data['基差率'], name='基差率', marker_color='rgb(244,128,68)')
-    #fig_basis_rate = go.Bar()
-    #fig_basis_rate.mode = 'markers'
-    # fig_basis_rate.x = df_rb0['日期']
-    # fig_basis_rate.y = df_rb0['基差率']
-    fig_basis_rate.marker.colorscale = ['green', 'red']
-    fig_basis_rate.marker.color = df_figure['基差率颜色']
-    #fig_basis_rate.marker.size = 2
-    fig_basis_rate.marker.showscale = False
-    fig_basis_rate.showlegend = False
+    # 创建辅图-基差率，并根据基差率正负配色
+    sign_color_mapping = {0:'green', 1:'red'}
+    df_figure = pd.DataFrame()
+    df_figure['基差率颜色'] = symbol.symbol_data['基差率'] > 0
+    df_figure['基差率颜色'] = df_figure['基差率颜色'].replace({True:1, False:0})
+    fig_basis_rate = go.Bar(x=symbol.symbol_data['日期'], y = symbol.symbol_data['基差率'], name='基差率',
+                            marker=dict(color=df_figure['基差率颜色'], colorscale=list(sign_color_mapping.values()),
+                                        showscale=False),
+                            showlegend=False,
+                            hovertemplate='%{y:.2%}')
     fig.add_trace(fig_basis_rate, row = 2, col = 1)
 
+    # 创建辅图-库存/仓单
     # fig_receipt = go.Scatter(x=symbol.symbol_data['日期'], y=symbol.symbol_data['仓单'], name='仓单', marker_color='rgb(239,181,59)')
     # fig_storage = go.Bar(x=symbol.symbol_data['日期'], y=symbol.symbol_data['库存'], name='库存', marker_color='rgb(234,69,70)')
     #fig_storage = px.bar(df_rb0, x='日期', y='库存')
     # fig.add_trace(fig_receipt, row = 3, col = 1, secondary_y=True)
     # fig.add_trace(fig_storage, row = 3, col = 1)
 
-    fig_receipt_rank = go.Scatter(x=df_rank['日期'], y=df_rank['仓单历史时间百分位'], name='仓单分位', marker_color='rgb(239,181,59)')
-    fig_storage_rank = go.Bar(x=df_rank['日期'], y=df_rank['库存历史时间百分位'], name='库存分位', marker_color='rgb(234,69,70)')
-    #fig_storage = px.bar(df_rb0, x='日期', y='库存')
-    fig_receipt_rank.showlegend = False
+    # 创建辅图-库存/仓单历史时间百分位，并根据分位配色
+    histroy_color_mapping ={1:'red', 2:'lightblue', 3:'lightblue', 4:'lightblue', 5:'green'}
+    # df_rank['仓单分位颜色'] = df_rank['仓单历史时间分位'].map(histroy_color_mapping)
+    # fig_receipt_rank = go.Scatter(x=df_rank['日期'], y=df_rank['仓单历史时间百分位'], name='仓单分位', marker_color='rgb(239,181,59)')
+    fig_receipt_rank = go.Scatter(x=df_rank['日期'], y=df_rank['仓单历史时间百分位'], name='仓单分位', mode='markers',
+                                marker=dict(size=2, color=df_rank['仓单历史时间分位'], colorscale=list(histroy_color_mapping.values())),
+                                showlegend=False,
+                                hovertemplate='%{y:.2%}')
     fig.add_trace(fig_receipt_rank, row = 3, col = 1, secondary_y=True)
+    df_rank['库存分位颜色'] = df_rank['库存历史时间分位'].map(histroy_color_mapping)
+    # fig_storage_rank = go.Bar(x=df_rank['日期'], y=df_rank['库存历史时间百分位'], name='库存分位', marker_color='rgb(234,69,70)')
+    fig_storage_rank = go.Bar(x=df_rank['日期'], y=df_rank['库存历史时间百分位'], name='库存分位', marker_color=df_rank['库存分位颜色'],
+                            hovertemplate='%{y:.2%}')
     fig.add_trace(fig_storage_rank, row = 3, col = 1)
 
+    # 创建辅图-现货利润/盘面利润
     # fig_spot_profit = go.Scatter(x=df_profit['日期'], y=df_profit['现货利润'], name='现货利润', marker_color='rgb(239,181,59)')
     # fig_future_profit = go.Bar(x=df_profit['日期'], y=df_profit['盘面利润'], name='盘面利润', marker_color='rgb(234,69,70)')
     # fig.add_trace(fig_spot_profit, row = 4, col = 1, secondary_y=True)
     # fig.add_trace(fig_future_profit, row = 4, col = 1)
 
-    fig_spot_profit = go.Scatter(x=df_rank['日期'], y=df_rank['现货利润历史时间百分位'], name='现货利润历史时间百分位', marker_color='rgb(239,181,59)')
-    fig_future_profit = go.Bar(x=df_rank['日期'], y=df_rank['盘面利润历史时间百分位'], name='盘面利润历史时间百分位', marker_color='rgb(234,69,70)')
-    fig_future_profit.showlegend = False
+    # 创建辅图-现货利润/盘面利润历史时间分位
+    df_rank['盘面利润分位颜色'] = df_rank['盘面利润历史时间分位'].map(histroy_color_mapping)
+    fig_spot_profit = go.Scatter(x=df_rank['日期'], y=df_rank['现货利润历史时间百分位'], name='现货利润', mode='markers',
+                                marker=dict(size=2, color=df_rank['现货利润历史时间分位'], colorscale=list(histroy_color_mapping.values())),
+                                hovertemplate='%{y:.2%}')
     fig.add_trace(fig_spot_profit, row = 4, col = 1, secondary_y=True)
+    fig_future_profit = go.Bar(x=df_rank['日期'], y=df_rank['盘面利润历史时间百分位'], name='盘面利润', marker_color=df_rank['盘面利润分位颜色'],
+                            showlegend=False,
+                            hovertemplate='%{y:.2%}')
     fig.add_trace(fig_future_profit, row = 4, col = 1)
 
+    # 根据交易时间过滤空数据
     trade_date = ak.tool_trade_date_hist_sina()['trade_date']
     trade_date = [d.strftime("%Y-%m-%d") for d in trade_date]
     dt_all = pd.date_range(start=symbol.symbol_data['日期'].iloc[0],end=symbol.symbol_data['日期'].iloc[-1])
@@ -227,10 +235,9 @@ def update_graph(col_chosen):
         ticklabelmode="period",   # instant  period
         tickformat="%b\n%Y",
         rangebreaks=[dict(values=dt_breaks)],
-        rangeslider_visible = False # 下方滑动条缩放
-        # 数据日期选择按钮
+        rangeslider_visible = False, # 下方滑动条缩放
+        # 增加固定范围选择
         # rangeselector = dict(
-        #     # 增加固定范围选择
         #     buttons = list([
         #         dict(count = 1, label = '1M', step = 'month', stepmode = 'backward'),
         #         dict(count = 6, label = '6M', step = 'month', stepmode = 'backward'),
@@ -240,12 +247,15 @@ def update_graph(col_chosen):
         #         ]))
     )
     #fig.update_traces(xbins_size="M1")
+    max_y = symbol.symbol_data['主力合约收盘价'] .max() * 1.05
+    min_y = symbol.symbol_data['主力合约收盘价'] .min() * 0.95
     fig.update_layout(
         yaxis_range=[min_y,max_y],
         #autosize=False,
         #width=800,
         height=1000,
         margin=dict(l=0, r=0, t=0, b=0),
+        hovermode='x unified',
         legend=dict(
             orientation='h',
             yanchor='bottom',
