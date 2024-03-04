52cf89a5f494d4b9af2a1d9d98619193b9c9b3df
Howard-66 <xxxxmouse@gmail.com>
2024-03-05 00:29:23 +0800

    adjusted main layout

--- chain.py
+++ chain.py
@@ -9,12 +9,12 @@ page_property = {
 
 # the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE2 = {
-    "position": "fixed",
-    "top": 60,
-    "left": 0,
+    # "position": "fixed",
+    "top": 150,
+    "left": -10,
     "bottom": 0,
-    "width": "16rem",
-    "padding": "2rem 1rem",
+    "width": "9rem",
+    "padding": "0rem 0rem",
     "background-color": "#f8f9fa",
 }
 
@@ -31,6 +31,11 @@ sidebar = html.Div(
                 dbc.NavLink("跨品种分析", href="/chain", active="exact"),
                 dbc.NavLink("交易计划", href="/plan", active="exact"),
                 dbc.NavLink("品种设置", href="/setting", active="exact"),
+                html.Hr(),
+                dbc.NavLink("产业链视图", href="/black_metal/chain", active="exact"),
+                dbc.NavLink("螺纹钢", href="/black_metal/RB", active="exact"),
+                dbc.NavLink("铁矿石", href="/black_metal/I", active="exact"),
+                dbc.NavLink("焦炭", href="/J", active="exact"),
             ],
             vertical=True,
             # pills=True,
@@ -38,7 +43,7 @@ sidebar = html.Div(
             card=True,
         ),
     ],
-    style=SIDEBAR_STYLE,
+    style=SIDEBAR_STYLE2,
 )
 
 tab_overview = html.Div([
@@ -53,13 +58,14 @@ tab_overview = html.Div([
 
 # 产业链下具体品种分析页面
 tab_basis = html.Div([
-    dbc.Card(    
-        dbc.CardBody(
-            [
-                html.P("basis")
-            ]
-        ),
-    ) 
+    dbc.Row([
+        # dbc.Col([
+        #     sidebar,
+        # ]),    
+        dbc.Col([
+            html.P("basis")
+        ])
+    ]),
 ])
 
 tab_cycle = html.Div([
@@ -72,13 +78,25 @@ tab_cycle = html.Div([
     ) 
 ])
 
+tab_content = dbc.Card(
+    dbc.CardBody(
+        [
+            
+        ]
+    ),
+    className="mt-3",
+)
+
 # 产业链分析tabview
 chain_tabs = dbc.Tabs(
     [
         dbc.Tab(label="产业链", tab_id="tab-overview", id="tab-overview"),
-        dbc.Tab(label="螺纹钢", tab_id="tab-symbol-rb", id='tab-symbol-rb'),
-        dbc.Tab(label="铁矿石", tab_id="tab-symbol-i", id="tab-symbol-i"),
-        dbc.Tab(label="焦炭", tab_id="tab-symbol-l"),
+        dbc.Tab(tab_content, label="基本面分析", tab_id="tab-main"),
+        dbc.Tab(tab_content, label="周期性分析", tab_id="tab-cycle"),
+        dbc.Tab(tab_content, label="跨期套利分析", tab_id="tab-time-cross"),
+        dbc.Tab(tab_content, label="跨品种分析", tab_id="tab-symbol-cross"),
+        dbc.Tab(tab_content, label="交易计划", tab_id="tab-trading-plan"),
+        dbc.Tab(tab_content, label="品种设置", tab_id="tab-config"),
     ],
     id="chain-tabs",
     active_tab="tab-overview",
@@ -96,34 +114,33 @@ page_layout = html.Div(
         dbc.Row([
             dbc.Col([
                 sidebar,
-            ],
-            width=2),
+            ], width=2),
             dbc.Col([
                 dbc.Row([
-                    chain_tabs,
+                    # chain_tabs,
                 ]),
-            ]),
+            ],width='12'),
         ]),
     ],
 )
 
 def layout(pathname):
     print(chain_tabs.active_tab, global_vars["active_tab"])
-    chain_tabs.active_tab = global_vars["active_tab"]
-    for child in chain_tabs.children:
-        if child.tab_id==global_vars["active_tab"]:
-            if pathname=='/basis':
-                print('on_basis')
-                child.children = tab_basis
-            elif pathname=='/cycle':
-                print('on_cycle')
-                child.children = tab_cycle
-            else:
-                child.children = {}
-        else:
-            child.children = {}
-        global_vars[chain_tabs.active_tab] = pathname
-    page_layout.children[1].children[1].children[0].children[0] = chain_tabs
+    # chain_tabs.active_tab = global_vars["active_tab"]
+    # for child in chain_tabs.children:
+    #     if child.tab_id==global_vars["active_tab"]:
+    #         if pathname=='/basis':
+    #             print('on_basis')
+    #             child.children = tab_basis
+    #         elif pathname=='/cycle':
+    #             print('on_cycle')
+    #             child.children = tab_cycle
+    #         else:
+    #             child.children = {}
+    #     else:
+    #         child.children = {}
+    #     global_vars[chain_tabs.active_tab] = pathname
+    # page_layout.children[1].children[1].children[0].children[0] = chain_tabs
     return page_layout
 
 # 初始化数据
@@ -142,20 +159,20 @@ def initialize():
     # symbol_chain.initialize_data(dws)
     return
 
-def callback(app):
-    @app.callback(
+# def callback(app):
+    # @app.callback(
         # Output("tab-overview", "children"), 
         # Output("tab-symbol-rb", "children"), 
-        Output("url", "pathname"), 
-        Input("chain-tabs", "active_tab"),
-    )
-    def tab_content(active_tab):
-        global_vars["active_tab"] = active_tab
-        print("on_clic_tab: ", global_vars["active_tab"])
-        if active_tab in global_vars:
-            return global_vars[active_tab]
-        else:
-            return "/basis"
+        # Output("url", "pathname"), 
+        # Input("chain-tabs", "active_tab"),
+    # )
+    # def tab_content(active_tab):
+        # global_vars["active_tab"] = active_tab
+        # print("on_clic_tab: ", global_vars["active_tab"])
+        # if active_tab in global_vars:
+        #     return global_vars[active_tab]
+        # else:
+        #     return "/basis"
         # if active_tab=="tab-overview":
         #     return tab_overview, {}, {}
         # elif active_tab=="tab-symbol-rb":
